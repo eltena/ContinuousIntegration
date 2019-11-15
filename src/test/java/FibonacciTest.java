6be12d835476d311d800f5b6c40b21a8fc76a2e6
eltena <42619604+eltena@users.noreply.github.com>
2019-11-15 00:02:52 -0500

    changed test

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -4,13 +4,8 @@ import org.junit.jupiter.api.Test;
 public class FibonacciTest
 {
     @Test
-    public void fibtest1()
-    {
+    public void fibtest1() {
         Assertions.assertEquals(1, Fibonacci.fibonacci(2));
-    }
-    @Test
-    public void test2() {
-        Assertions.assertEquals(4, Fibonacci.fibonacci(5));
     }
 
 }
