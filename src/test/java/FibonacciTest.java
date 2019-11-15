3b5e3173ee9c37a3e47d18788e39022d6749ee2c
eltena <42619604+eltena@users.noreply.github.com>
2019-11-15 00:05:40 -0500

    Added Second test

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -7,5 +7,10 @@ public class FibonacciTest
     public void fibtest1() {
         Assertions.assertEquals(1, Fibonacci.fibonacci(2));
     }
+    @Test
+    public void fibtest2() {
+        Assertions.assertEquals(4, Fibonacci.fibonacci(5));
+    }
+
 
 }
